5ab87a8ba05d96ec1ca02d336b6642290bc6d5da
DeepshikaGhale <deepshikaghale99@gmail.com>
2021-12-05 16:47:54 +0545

    ui update

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -5,31 +5,26 @@ import 'package:news_api_flutter/Model/newsinfo.dart';
 import 'package:news_api_flutter/constants/strings.dart';
 
 class APIManager{
-
-  Future<NewsModelClass> getNews() async{
+  Future<NewsModelClass> getNews()async{
     var client = http.Client();
 
     var newsModel;
-    
-    try{
-    //catching the data from the server
-    var response = await client.get(Uri.parse(Strings.news_url));
 
+    try{
+      var response = await client.get(Uri.parse(Strings.news_url));
+    
     if(response.statusCode == 200){
-       print('inside 200');
       var jsonString = response.body;
-       print('jsonString $jsonString');
-      var jsonMap = json.decode(jsonString); //decoding the above jsonString
-       print('object is $jsonMap');
+
+      var jsonMap = json.decode(jsonString);
+
       newsModel = NewsModelClass.fromJson(jsonMap);
-       print('${newsModel}');
     }
     }catch(Exception){
-      print(Exception);
-      return newsModel;
+        return newsModel;
     }
-    print(newsModel);
-    return newsModel;
     
+    
+    return newsModel;
   }
 }
